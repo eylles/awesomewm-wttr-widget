bd267797bf9c97ab96cd3ea1d69f6d8b97a71e7f
eylles <ed.ylles1997@gmail.com>
2026-03-22 22:02:12 -0600

    Set cache_path in init.lua

--- signal/init.lua
+++ signal/init.lua
@@ -1,10 +1,13 @@
 local awful = require("awful")
+local filesystem = require("gears.filesystem")
+
+local cache_path = filesystem.get_cache_dir() .. "wttr"
 
 local currentPath = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
 
-local leds_daemon = currentPath .. "awesome-wttr.sh"
+local weather_daemon = currentPath .. "awesome-wttr.sh " .. cache_path
 
-local pid = awful.spawn(leds_daemon, false)
+local pid = awful.spawn(weather_daemon, false)
 
 awesome.connect_signal("exit",
     function()
